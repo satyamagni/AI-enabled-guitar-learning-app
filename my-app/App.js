b88cd1c4a70ba8785c62de73b67bb6d1c91ce5fd
Satyam <satyamagnihotri97@gmail.com>
2021-08-05 17:31:41 -0400

    added a working image picker functionality

--- my-app/App.js
+++ my-app/App.js
@@ -1,9 +1,10 @@
 import 'react-native-gesture-handler';
 import React, {useState, useEffect} from 'react';
-import { RadioButton, TextInput } from 'react-native-paper';
+import { Avatar, RadioButton, TextInput } from 'react-native-paper';
+import { Audio } from 'expo-av'
 import { Camera } from 'expo-camera';
 import * as ImagePicker from 'expo-image-picker';
-import ImagePickerExample from "./camera.js"
+//import ImagePickerExample from "./camera.js"
 
 
 import {
@@ -113,6 +114,31 @@ const usernameScreen = ({navigation}) => {
     );
 };
 const HomeScreen = ({navigation}) => {
+    const [selectedImage, setSelectedImage] = React.useState(null);
+    const ImagePickerAsync = async () => {
+      let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (permissionResult.granted === false) {
+        alert("Permission to access camera roll is required!");
+        return;
+      }
+      let pickerResult = await ImagePicker.launchImageLibraryAsync();
+      console.log(pickerResult);
+      if (pickerResult.cancelled === true) {
+        return;
+      }
+      setSelectedImage({ localUri: pickerResult.uri });
+    }
+    if (selectedImage !== null) {
+      return (
+        <View style={styles.container}>
+          <Image
+            source={{ uri: selectedImage.localUri }}
+            style={styles.thumbnail}
+          />
+        </View>
+      );
+    }
+
     return(
         <SafeAreaView styles={styles.container1}>
             <View style={{alignItems:'center'}}>
@@ -133,91 +159,12 @@ const HomeScreen = ({navigation}) => {
                     <Text>Practice music scores</Text>
                 </View>
             <View style={{width:'50%',height: 250, backgroundColor:'dodgerblue', justifyContent:'center', alignItems:'center'}}>
-                    <Button title='Upload image score' onClick={()=> ImagePickerExample}/>
+                    <Button title='Upload image score' onPress={ImagePickerAsync}/>
             </View>
             </View>
         </SafeAreaView>
     );
 };
-
-/*
-<Button title='Upload image score' onPress={()=> ImagePickerExample()}/>
-const function UploadImage() {
-    const [image, setImage] = useState(null);
-  
-    useEffect(() => {
-      (async () => {
-        if (Platform.OS !== 'web') {
-          const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-          if (status !== 'granted') {
-            alert('Sorry, we need camera roll permissions to make this work!');
-          }
-        }
-      })();
-    }, []);
-  
-    const pickImage = async () => {
-      let result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.All,
-        allowsEditing: true,
-        aspect: [4, 3],
-        quality: 1,
-      });
-  
-      console.log(result);
-  
-      if (!result.cancelled) {
-        setImage(result.uri);
-      }
-    };
-  
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Button title="Pick an image from camera roll" onPress={pickImage} />
-        {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
-      </View>
-    );
-  }
-*/
-
-/*
-function OpenCamera () {
-    const [hasPermission, setHasPermission] = useState(null);
-    const [type, setType] = useState(Camera.Constants.Type.back);
-    useEffect(() => {
-        (async () => {
-          const { status } = await Camera.requestPermissionsAsync();
-          setHasPermission(status === 'granted');
-        })();
-      }, []);
-      if (hasPermission === null) {
-        return <View />;
-      }
-      if (hasPermission === false) {
-        return <Text>No access to camera</Text>;
-      }
-      return (
-        <View style={styles.container}>
-          <Camera style={styles.camera} type={type}>
-            <View style={styles.buttonContainer}>
-              <TouchableOpacity
-                style={styles.button}
-                onPress={() => {
-                  setType(
-                    type === Camera.Constants.Type.back
-                      ? Camera.Constants.Type.front
-                      : Camera.Constants.Type.back
-                  );
-                }}>
-                <Text style={styles.text}> Flip </Text>
-              </TouchableOpacity>
-            </View>
-          </Camera>
-        </View>
-      );   
-
-}
-*/
 
 const styles = StyleSheet.create(
     {
@@ -228,7 +175,13 @@ const styles = StyleSheet.create(
     },
     container3:{},
     container: {flex:1},
-    camera : {  }
+    camera : {  },
+    thumbnail: {
+      flex: 1,
+      width: null,
+      height: null,
+      resizeMode: "contain"
+      }
     }
 );
 export default App;
